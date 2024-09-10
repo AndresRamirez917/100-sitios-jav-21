87eae71a83bfa5e8f7d128195bde5da49a4ee1b6
AndresRamirez917 <andresmintic2021@gmail.com>
2024-09-10 12:00:28 -0500

    first commit

--- js/index.js
+++ js/index.js
@@ -21,10 +21,53 @@ async function getData() {
             }
         }
     });
-    
+
     function randonImage(min, max){
         return Math.floor(Math.random() * (max - min + 1) + min)
     }
 }
 
+
+
+btn_validar = document.getElementById('btn-validar')
+
+const validar = (e) => {
+    e.preventDefault();
+    const nombre = document.getElementById('nombre');
+    const email = document.getElementById('email');
+    const mensaje = document.getElementById('mensaje');
+    const arr = [];
+    const arrMessages = ["Nombre", "Email", "Mensaje"];
+    arr.push(nombre, email, mensaje);
+    for(i = 0; i < arr.length; i++){
+        if(arr[i].value == ""){
+            swal({
+                title: `El campo ${arrMessages[i]} no puede estar vacío`,
+                icon: "error",
+                 })
+                 return false;
+        }
+    }
+    if(!emailValido(email.value)){
+        swal({
+            title: `El campo ${arrMessages[1]} no tiene el formato correcto`,
+            icon: "error",
+             })
+             return false;
+    }
+    swal({
+        title: `Datos enviados satisfactoriamente`,
+        icon: "success",
+         })
+         nombre.value = "";
+         email.value = "";
+         mensaje.value = "";
+    return true;
+}
+
+const emailValido = email => {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)
+}
+
+btn_validar.addEventListener("click", validar)
 getData();
